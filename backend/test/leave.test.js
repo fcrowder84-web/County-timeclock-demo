55daa7cce0862a50e8008cfa5243a6a66de176dd
fcrowder84-web <fcrowder84@gmail.com>
2026-08-17 15:20:40 -0400

    Test flexible holiday hours for varied schedules

--- backend/test/leave.test.js
+++ backend/test/leave.test.js
@@ -8,7 +8,7 @@ const {
   validateFloatingHolidayRequest,
   LEAVE_TYPES,
 } = require('../routes/leave');
-const { getHolidayCalendar, findFixedHoliday, isWorkday } = require('../lib/holiday-calendar');
+const { getHolidayCalendar, findFixedHoliday, isWorkday, FLOATING_HOLIDAY_POLICY } = require('../lib/holiday-calendar');
 
 assert.strictEqual(parseQuarterHours(0.25), 1);
 assert.strictEqual(parseQuarterHours(4), 16);
@@ -27,12 +27,14 @@ assert.strictEqual(findFixedHoliday('2026-12-28').name, 'Christmas');
 assert.strictEqual(findFixedHoliday('2026-08-17'), null);
 assert.strictEqual(isWorkday('2026-08-17'), true);
 assert.strictEqual(isWorkday('2026-08-16'), false);
+assert.strictEqual(FLOATING_HOLIDAY_POLICY.hours_flexible, true);
 assert.doesNotThrow(() => validateFixedHolidayDates(['2026-01-01','2026-12-28']));
 assert.throws(() => validateFixedHolidayDates(['2026-08-17']), /County holiday calendar/);
-assert.doesNotThrow(() => validateFloatingHolidayRequest(['2026-08-17'], 32));
-assert.throws(() => validateFloatingHolidayRequest(['2026-08-16'], 32), /workday/);
-assert.throws(() => validateFloatingHolidayRequest(['2026-08-17'], 16), /8-hour/);
-assert.throws(() => validateFloatingHolidayRequest(['2026-08-17','2026-08-18'], 32), /one workday/);
+assert.doesNotThrow(() => validateFloatingHolidayRequest(['2026-08-17']));
+assert.throws(() => validateFloatingHolidayRequest(['2026-08-16']), /workday/);
+assert.throws(() => validateFloatingHolidayRequest(['2026-08-17','2026-08-18']), /one workday/);
+assert.strictEqual(parseQuarterHours(7), 28);
+assert.strictEqual(parseQuarterHours(10), 40);
 
 let check = assessDailyPaidHours({ workedQuarterHours: 8, proposedQuarterHours: 24 });
 assert.strictEqual(check.total_hours, 8);
