4215204853e8547efa13a1f030440cc6bce5b211
fcrowder84-web <fcrowder84@gmail.com>
2026-09-25 09:42:00 -0400

    Test trusted-network GPS punch gate

--- backend/test/punch-metadata.test.js
+++ backend/test/punch-metadata.test.js
@@ -1,6 +1,6 @@
 'use strict';
 const assert=require('assert');
-const {getRequestIp,normalizeLocation,normalizeClientSource}=require('../lib/punch-metadata');
+const {getRequestIp,normalizeLocation,normalizeClientSource,punchLocationGate}=require('../lib/punch-metadata');
 assert.strictEqual(getRequestIp({headers:{'cf-connecting-ip':'203.0.113.8'},socket:{remoteAddress:'172.18.0.2'}}),'203.0.113.8');
 assert.strictEqual(getRequestIp({headers:{'x-forwarded-for':'198.51.100.4, 172.18.0.3'},socket:{remoteAddress:'172.18.0.2'}}),'198.51.100.4');
 assert.strictEqual(getRequestIp({headers:{},socket:{remoteAddress:'::ffff:192.0.2.5'}}),'192.0.2.5');
@@ -9,4 +9,7 @@ assert.deepStrictEqual(normalizeLocation({location_status:'captured',latitude:99
 assert.deepStrictEqual(normalizeLocation({location_status:'denied'}),{location_status:'denied',latitude:null,longitude:null,accuracy_meters:null});
 assert.strictEqual(normalizeClientSource('mobile_pwa'),'mobile_pwa');
 assert.strictEqual(normalizeClientSource('bad source!'),'web');
+assert.strictEqual(punchLocationGate({headers:{'cf-connecting-ip':'64.139.245.9'},body:{location_status:'denied'}}).allowed,true);
+assert.strictEqual(punchLocationGate({headers:{'cf-connecting-ip':'192.0.2.25'},body:{location_status:'denied'}}).allowed,false);
+assert.strictEqual(punchLocationGate({headers:{'cf-connecting-ip':'192.0.2.25'},body:{location_status:'captured',latitude:33.77,longitude:-81.93,accuracy_meters:12}}).allowed,true);
 console.log('punch-metadata tests: PASS');
